edd72b6cf873348f6062ca6e07b26aba1ec5f593
ludolpif <ludolpif@gmail.com>
2025-11-30 16:08:38 +0100

    Rename plugin to mod

--- mods/llss-mod-template/src/hello.c
+++ mods/llss-mod-template/src/hello.c
@@ -1,4 +1,4 @@
-#include "plugin.h"
+#include "mod.h"
 
 Sint32 hello(Sint32 b) {
 	return some_private_func(b);
--- mods/llss-mod-template/src/hello.h
+++ mods/llss-mod-template/src/hello.h
@@ -1,6 +1,4 @@
 #pragma once
-
 #include "app.h"
-#include "plugin.h"
 
 Sint32 hello(Sint32 b);
