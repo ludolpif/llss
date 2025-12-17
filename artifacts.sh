8c9a5d00d30ad8adb7ad7fb0b41971505d5e2816
ludolpif <ludolpif@gmail.com>
2025-12-17 11:32:27 +0100

    build: one script for one task, thinking CI integration.

--- artifacts.sh
+++ artifacts.sh
@@ -1,23 +1,30 @@
-#!/bin/sh -xe
+#!/bin/sh
 MAKE="make -j"
 
 name=$(./get --name)
 version=$(./get --version)
 prettyos=$(./get --prettyos)
 
-artifact_sources=artifacts/$name-$version-Sources
+mkarchive_sources() {
+	target=artifacts/$name-$version-Sources
+	set -xe
+	git ls-files > $target.lst
+	rm -f $target.zip
+	zip -r $target.zip . -i@$target.lst
+	exit $?
+}
 
-./configure
-mkdir -p artifacts
-git ls-files > $artifact_sources.lst
+mkarchive_bin() {
+	target=artifacts/$name-$version-$prettyos-$1
+	set -xe
+	rm -f $target.zip
+	zip -r $target.zip doc/user program/x64/$1/llss mods/*/program/x64/$1/*.so program/data mods/*/data
+	exit $?
+}
 
-rm -f $artifact_sources.zip
-zip -qr $artifact_sources.zip . -i@$artifact_sources.lst
-
-for b in Debug Release
-do
-	artifact_build=artifacts/$name-$version-$prettyos-$b
-	$MAKE all BUILD_TYPE=$b
-	rm -f $artifact_build.zip
-	zip -qr $artifact_build.zip doc/user program/x64/$b/llss mods/*/program/x64/$b/*.so program/data mods/*/data
-done
+case $1 in
+	Sources) mkdir -p artifacts && mkarchive_sources ;;
+	Debug)   mkdir -p artifacts && mkarchive_bin $1 ;;
+	Release) mkdir -p artifacts && mkarchive_bin $1 ;;
+	*) echo "Usage $0 (Sources|Debug|Release)" >&2; exit 1;;
+esac
