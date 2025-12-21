2d1bc15a75a1b3743b95e483f0a49c798e2981d3
ludolpif <ludolpif@gmail.com>
2025-12-21 22:51:54 +0100

    Licence: add notices

--- include/app.h
+++ include/app.h
@@ -1,7 +1,22 @@
 #pragma once
-
 /*
- * Our public API. Kept in one file to help searching... but long, so quick table of content.
+ * This file is part of LLSS.
+ *
+ * LLSS is free software: you can redistribute it and/or modify it under the terms of the
+ * Affero GNU General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * LLSS is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with LLSS.
+ * If not, see <https://www.gnu.org/licenses/>. See LICENSE file at root of this git repo.
+ *
+ * Copyright 2025 ludolpif <ludolpif@gmail.com>
+ *
+ * This file is our main program public API. mods can #include it and use everything.
+ * Kept in one file to help searching... but long, so quick table of content.
  * Note: flecs.h and dcimgui.h do the same, check them too. For SDL3, check this link:
  * https://wiki.libsdl.org/SDL3/CategoryAPI
  *
--- include/metadata.h
+++ include/metadata.h
@@ -9,7 +9,7 @@
 #define APP_METADATA_NAME_STRING "llss"
 #define APP_METADATA_IDENTIFIER_STRING "fr.ludolpif.llss.app"
 #define APP_METADATA_CREATOR_STRING "ludolpif" // Also used as Windows prop "CompanyName"
-#define APP_METADATA_COPYRIGHT_STRING "Copyright (c) 2025-now ludolpif <ludolpif@gmail.com>"
+#define APP_METADATA_COPYRIGHT_STRING "Copyright 2025 ludolpif <ludolpif@gmail.com>"
 #define APP_METADATA_URL_STRING "https://ludolpif.fr/llss"
 #define APP_METADATA_TYPE_STRING "application"
 #define APP_METADATA_DESCRIPTION "Libre Live Streaming Software (LLSS) for Streamers making live brodcasts on platforms like twitch.tv"
