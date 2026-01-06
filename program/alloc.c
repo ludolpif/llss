b0ceb8ea033f603a2f9e3444f1bba7e1e2f7d829
ludolpif <ludolpif@gmail.com>
2026-01-06 10:06:11 +0100

    alloc: fix naive malloc/calloc/realloc/free accounting

--- program/alloc.c
+++ program/alloc.c
@@ -74,55 +74,106 @@ void alloc_count_set_context(app_alloc_count_contexts_t contextid) {
 }
 
 void * SDLCALL alloc_count_malloc(size_t size) {
-    SDL_AtomicIncRef(alloc_count_current_context+0);
+    if (size) {
+        SDL_AtomicIncRef(alloc_count_current_context+0);
+    } else {
+        app_debug("%016"PRIu64" alloc_count_malloc(0) called", SDL_GetTicksNS());
+    }
     return orig_malloc_func(size);
 }
 
 void * SDLCALL alloc_count_malloc_ecs(ecs_size_t size) {
-    SDL_AtomicIncRef(alloc_count_current_context+0);
+    if (size) {
+        SDL_AtomicIncRef(alloc_count_current_context+0);
+    } else {
+        app_debug("%016"PRIu64" alloc_count_malloc_ecs(0) called", SDL_GetTicksNS());
+    }
     return orig_malloc_func(size);
 }
 
 void * SDLCALL alloc_count_malloc_userptr(size_t size, void *userptr) {
     (void) userptr;
-    SDL_AtomicIncRef(alloc_count_current_context+0);
+    if (size) {
+        SDL_AtomicIncRef(alloc_count_current_context+0);
+    } else {
+        app_debug("%016"PRIu64" alloc_count_malloc_userptr(0) called", SDL_GetTicksNS());
+    }
     return orig_malloc_func(size);
 }
 
 void * SDLCALL alloc_count_calloc(size_t nmemb, size_t size) {
-    SDL_AtomicIncRef(alloc_count_current_context+1);
+    if (nmemb && size) {
+        SDL_AtomicIncRef(alloc_count_current_context+1);
+    } else if (!nmemb) {
+        app_debug("%016"PRIu64" alloc_count_calloc(0, size) called", SDL_GetTicksNS());
+    } else if (!size) {
+        app_debug("%016"PRIu64" alloc_count_calloc(nmemb, 0) called", SDL_GetTicksNS());
+    }
     return orig_calloc_func(nmemb, size);
 }
 
 void * SDLCALL alloc_count_calloc_ecs(ecs_size_t size) {
-    SDL_AtomicIncRef(alloc_count_current_context+1);
+    if (size) {
+        SDL_AtomicIncRef(alloc_count_current_context+1);
+    } else {
+        app_debug("%016"PRIu64" alloc_count_calloc_ecs(0) called", SDL_GetTicksNS());
+    }
     return orig_calloc_func(1, size);
 }
 
 void * SDLCALL alloc_count_realloc(void *mem, size_t size) {
-    SDL_AtomicIncRef(alloc_count_current_context+2);
-    return orig_realloc_func(mem, size);
+    void *new = orig_realloc_func(mem, size);
+    if (mem && size) {
+        SDL_AtomicIncRef(alloc_count_current_context+2);
+    } else if (!size) {
+        app_debug("%016"PRIu64" alloc_count_realloc(mem, 0) called", SDL_GetTicksNS());
+        SDL_AtomicIncRef(alloc_count_current_context+3);
+    } else if (!mem) {
+        app_debug("%016"PRIu64" alloc_count_realloc(NULL, size) called", SDL_GetTicksNS());
+        SDL_AtomicIncRef(alloc_count_current_context+0);
+    }
+    return new;
 }
 
 void * SDLCALL alloc_count_realloc_ecs(void *mem, ecs_size_t size) {
-    SDL_AtomicIncRef(alloc_count_current_context+2);
-    return orig_realloc_func(mem, size);
+    void *new = orig_realloc_func(mem, size);
+    if (mem && size) {
+        SDL_AtomicIncRef(alloc_count_current_context+2);
+    } else if (!size) {
+        app_debug("%016"PRIu64" alloc_count_realloc_ecs(mem, 0) called", SDL_GetTicksNS());
+        SDL_AtomicIncRef(alloc_count_current_context+3);
+    } else if (!mem) {
+        app_debug("%016"PRIu64" alloc_count_realloc_ecs(NULL, size) called", SDL_GetTicksNS());
+        SDL_AtomicIncRef(alloc_count_current_context+0);
+    }
+    return new;
 }
 
 void   SDLCALL alloc_count_free(void *mem) {
-    SDL_AtomicIncRef(alloc_count_current_context+3);
-    orig_free_func(mem);
+    if (mem) {
+        SDL_AtomicIncRef(alloc_count_current_context+3);
+        orig_free_func(mem);
+    } else {
+        app_debug("%016"PRIu64" alloc_count_free(NULL) called", SDL_GetTicksNS());
+    }
 }
 
 void   SDLCALL alloc_count_free_ecs(void *mem) {
-    SDL_AtomicIncRef(alloc_count_current_context+3);
-    orig_free_func(mem);
+    if (mem) {
+        SDL_AtomicIncRef(alloc_count_current_context+3);
+        orig_free_func(mem);
+    }
+    // no else { app_debug(...); } because flecs make many free(NULL) calls
 }
 
 void   SDLCALL alloc_count_free_userptr(void *mem, void *userptr) {
     (void) userptr;
-    SDL_AtomicIncRef(alloc_count_current_context+3);
-    orig_free_func(mem);
+    if (mem) {
+        SDL_AtomicIncRef(alloc_count_current_context+3);
+        orig_free_func(mem);
+    } else {
+        app_debug("%016"PRIu64" alloc_count_free_ecs_userptr(NULL) called", SDL_GetTicksNS());
+    }
 }
 
 char * SDLCALL alloc_count_strdup_ecs(const char *s) {
